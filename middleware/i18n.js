f6a99a75e74aee88993378376d8af91101c44390
Guli <ignaciomorenopubul@gmail.com>
2019-01-27 14:13:44 +0100

    Static site language test

--- middleware/i18n.js
+++ middleware/i18n.js
@@ -3,15 +3,19 @@
  * 2. redirects if not with locale
  */
 export default function ({ 
-  isHMR, app, store, route, params, error, redirect, req
+  isHMR, app, store, route, params, error, redirect
 }) {
   var locale
   var acceptedLocales = ['es', 'en']
-  var userLocale = req.headers['accept-language'].split(',')[0].toLocaleLowerCase().substring(0, 2)
+  if (process.browser) {
+    var userLocale = navigator.language.toLocaleLowerCase().substring(0, 2)
+    console.log('Locale:', userLocale)
+  }
   if (isHMR) { // ignore if called from hot module replacement
     return;
   } // if url does not have language, redirect to english
   else if (!params.lang) {
+    console.log(userLocale)
     locale = acceptedLocales.includes(userLocale) ? userLocale : 'en'
     return redirect('/' + locale +route.fullPath);
   }
